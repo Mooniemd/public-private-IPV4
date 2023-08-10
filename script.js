ab445e1419b4acb8b3771a733d28181fec77b4d6
Mooniemd <doragondoshiro@gmail.com>
2023-08-10 13:27:14 -0400

    feat: added categories to results

--- script.js
+++ script.js
@@ -10,23 +10,23 @@ function Calculo(){
     let mascara = '';
     let endereco = '';
     if(first >=1 && first <=127){
-        respostaFinal = "Classe A"
-        mascara = "255.0.0.0"
+        respostaFinal = "Classe: A"
+        mascara = "Máscara: 255.0.0.0"
         if(first <= 10){
-            endereco = "Privado"
-        } else (endereco = "Público")
+            endereco = "Endereço: Privado"
+        } else (endereco = "Endereço: Público")
     }else if(first >= 128 && first <= 191 && second <= 255){
-        respostaFinal = "Classe B"
-        mascara = "255.255.0.0"
+        respostaFinal = "Classe: B"
+        mascara = " Máscara: 255.255.0.0"
         if (first = 172 && second >= 16 && second <= 31 && third >=0 && fourth >= 0){
-            endereco = "Privado"
-        } else (endereco = "Público")
+            endereco = "Endereço: Privado"
+        } else (endereco = "Endereço: Público")
     }else if(first >= 192 && first <= 223 && second <= 255 && third <= 255 && fourth <= 255){
-        respostaFinal = "Classe C"
-        mascara = "255.255.255.0"
+        respostaFinal = "Classe: C"
+        mascara = "Máscara: 255.255.255.0"
         if (first = 192 && second == 168 && third >= 0 && fourth >= 0){
-            endereco = "Privado"
-        } else ( endereco = "Público")
+            endereco = "Endereço: Privado"
+        } else ( endereco = "Endereço Público")
     } else (respostaFinal = "IPV4 Inválido");
     resposta.innerHTML = respostaFinal + "\n" + mascara + "\n" + endereco;
 }
